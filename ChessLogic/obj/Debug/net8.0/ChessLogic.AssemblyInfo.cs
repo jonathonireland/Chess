a5bd4ed4edd2baea1c1248e25277d4d24e3b6e22
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-25 03:26:59 -0400

    currently stuck on error with readonly PiecePositionsFor

--- ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90f4020d73802ea17b8c05276e590aac0927e58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c6a6c62f5000c9355f46a359dcf4ccdc1317989")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
